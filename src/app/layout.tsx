91238354ba98388e3bd7c63545d3673c49b1cd31
ademosky <120837622+ademosky@users.noreply.github.com>
2026-07-16 01:31:13 +0200

    Set favicon to logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,11 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: "/images/logo.png",
+    shortcut: "/images/logo.png",
+    apple: "/images/logo.png",
+  },
 };
 
 export default function RootLayout({
