4bcedecd047f1f1d8cc7e9e68ad067f81cfdfd9d
ademosky <120837622+ademosky@users.noreply.github.com>
2026-07-16 09:55:13 +0200

    GA4: use next/script instead of third-parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Barlow_Condensed, DM_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import { CartProvider } from "./context/CartContext";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 import FacebookFloat from "./components/FacebookFloat";
 
 const barlowCondensed = Barlow_Condensed({
@@ -80,7 +80,15 @@ export default function RootLayout({
           <Footer />
           <FacebookFloat />
           {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
-            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
+            <>
+              <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+                strategy="afterInteractive"
+              />
+              <Script id="ga-init" strategy="afterInteractive">
+                {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}',{page_path:window.location.pathname});`}
+              </Script>
+            </>
           )}
         </CartProvider>
       </body>
