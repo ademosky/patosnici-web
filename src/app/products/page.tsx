e282d2cd0fd73bba84a1e2b20892d2c248275dcf
ademosky <120837622+ademosky@users.noreply.github.com>
2026-09-07 19:05:47 +0200

    products page now shows rubber_mats only

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,10 @@
-import { getProducts } from "../data/products";
+import { getProductsByCategory } from "../data/products";
 import { brands } from "../data/brands";
 import ProductsClient from "./ProductsClient";
 
 export const dynamic = "force-dynamic";
 
 export default async function ProductsPage() {
-  const products = await getProducts();
+  const products = await getProductsByCategory("rubber_mats");
   return <ProductsClient initialProducts={products} brands={brands} />;
 }
